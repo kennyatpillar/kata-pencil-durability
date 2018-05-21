46c92d1391c02b550c190419297dad876b98a1eb
Kenny Drobnack <kdrobnack@pillartechnology.com>
2018-05-21 17:05:32 -0400

    Handle appending text and made writeSomething return current writings

--- src/writer.js
+++ src/writer.js
@@ -1,5 +1,14 @@
 var writings = ''
 
+exports.clearWritings = function() {
+    writings = ''
+}
+
 exports.getWritings = function() {
+    return writings
+}
+
+exports.writeSomething = function(newWritings) {
+    writings = writings + newWritings
     return writings
 }
--- test/writer.js
+++ test/writer.js
@@ -2,9 +2,30 @@ var assert = require('assert');
 var writer = require('../src/writer.js');
 
 describe('Writer', function() {
+  beforeEach(function() {
+    writer.clearWritings()
+  })
+
   describe('before writing', function() {
     it('should return empty string if no writing has been done yet', function() {
       assert.equal(writer.getWritings(), '');
     });
+  });
+  describe('when writing', function() {
+    it('writing something should return writings', function() {
+      assert.equal(writer.writeSomething('foo'), 'foo');
+    });
+    it('clear writing some remove any existing writings', function() {
+      writer.writeSomething('clear me')
+      writer.clearWritings()
+      assert.equal(writer.getWritings(), '')
+    })
+    it('writing something should update writings', function() {
+      assert.equal(writer.writeSomething('something'), 'something')
+    })
+    it('additional writings should append to previous writings', function() {
+      writer.writeSomething('something')
+      assert.equal(writer.writeSomething(' else'), 'something else')
+    })
   });
 });
